49d6bcadf7ddb2efd6def33c6143414376135882
leeminjung1 <vipminjung@gmail.com>
2022-04-04 16:16:01 +0900

    Add MemberRepository, MemberService

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("lee");
+
+        // when
+        Long joinId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(joinId));
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("lee");
+        Member member2 = new Member();
+        member2.setName("lee");
+
+        // when
+        memberService.join(member1);
+        memberService.join(member2);
+        
+        // then
+        fail("중복회원발생");
+    }
+
+
+
+}
