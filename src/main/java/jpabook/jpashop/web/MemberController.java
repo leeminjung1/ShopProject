13a7cb352d4b814af9db7be0e3b5908c7baaa380
leeminjung1 <vipminjung@gmail.com>
2022-04-06 23:39:00 +0900

    Add MemberController

---
+++ src/main/java/jpabook/jpashop/web/MemberController.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.web;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    private String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    private String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        try {
+            Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+            Member member = new Member();
+            member.setName(form.getName());
+            member.setAddress(address);
+
+            memberService.join(member);
+
+            return "redirect:/";
+        } catch (IllegalStateException e) {
+            result.addError(new FieldError("memberForm", "name", e.getMessage()));
+            return "members/createMemberForm";
+        }
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+}
