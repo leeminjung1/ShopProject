f62f16f6a654b5a60223977668df74f3298b4973
leeminjung1 <vipminjung@gmail.com>
2022-04-11 23:58:32 +0900

    Mod itemform

--- src/main/java/jpabook/jpashop/web/BookForm.java
+++ src/main/java/jpabook/jpashop/web/BookForm.java
@@ -3,15 +3,24 @@ package jpabook.jpashop.web;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter @Setter
 public class BookForm {
 
     private Long id;
 
+    private List<String> itemCategory = new ArrayList<>();
     private String name;
     private int price;
     private int stockQuantity;
     private String author;
     private String isbn;
 
+    public BookForm() {
+        itemCategory.add("Album");
+        itemCategory.add("Book");
+        itemCategory.add("Movie");
+    }
 }
